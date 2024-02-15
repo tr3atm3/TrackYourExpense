69ddde3d8d15f34810873810d8f62da04fb082f2
tr3atm3 <tttushar.789@gmail.com>
2024-02-15 16:47:14 +0530

    boilerPlate

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>27 Match</div>
+      <div>
+        <h2>Rent</h2>
+        <div>$100</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
