e42537babb9678cdcbc85951a0e91b972a5988af
tr3atm3 <tttushar.789@gmail.com>
2024-02-16 01:43:42 +0530

    DeleteBtnAdded

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,12 @@ import { ExpenseDate } from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
+  const deleteHandler = (e) => {
+    e.target.parentElement.style.display = "none";
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +17,8 @@ const ExpenseItem = (props) => {
         locationOfExpenditure={props.locationOfExpenditure}
         title={props.title}
       />
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
     </Card>
   );
 };
