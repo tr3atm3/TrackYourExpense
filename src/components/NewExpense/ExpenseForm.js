ba5f629c5f4759066ae76436e6fa5fb1d7e198d7
tr3atm3 <tttushar.789@gmail.com>
2024-02-16 04:58:12 +0530

    eventHandlerAddedToInputField

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const titleChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+  const amountChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label for="title">Title</label>
+          <input type="text" id="title" onChange={titleChangeHandler}></input>
+        </div>
+        <div className="new-expense__control">
+          <label for="amount">Amount</label>
+          <input
+            type="number"
+            id="amount"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          ></input>
+        </div>
+        <div className="new-expense__control">
+          <label for="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          ></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
