c83ae42e917d92fe39e576e9ec645945161eb1f9
tr3atm3 <tttushar.789@gmail.com>
2024-02-16 21:09:51 +0530

    chartAdded

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = ({ expenses }) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -20,6 +21,7 @@ const Expenses = ({ expenses }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
       {filteredExpenses.length === 1 && (
         <p style={{ color: "white" }}>
